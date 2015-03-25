93e81b252be18fb72a2e7acbcf121c1f442381e3
Tsuyoshi Okumura <qkrhn081@gmail.com>
2015-03-25 15:06:13 +0900

    add xml parsing.

--- app/controllers/generators_controller.rb
+++ app/controllers/generators_controller.rb
@@ -2,7 +2,7 @@ class GeneratorsController < ApplicationController
   def index
   end
 
-  Member = Struct.new(:classname, :fields, :methods)
+  Member = Struct.new(:class, :methods)
 
   def is_target_member? attr_name
     #return true if attr_name.include?("SharedLibrary.Model")
@@ -16,63 +16,16 @@ class GeneratorsController < ApplicationController
     xml = params[:xml].read
     doc = Nokogiri::XML(xml)
 
-    class_list = Array.new
-    is_first = true
+    @class_list = Array.new
+    all_method_list = Array.new
 
-    doc.xpath("//member").each do |m|
-
-      attr_name = m.attribute("name").to_s
-
-      if attr_name.include?("T:")
-        if is_first
-          is_first = false
-        elsif is_target_member?(attr_name)
-          @member.fields = @fields
-          @member.methods = @methods
-          class_list << @member
-        end
-
-        @member = Member.new(attr_name.gsub(/T:/, ""), nil, nil)
-        @fields = Array.new
-        @methods = Array.new
-
-      elsif attr_name.include?("F:")
-        @fields << m
-
-      elsif attr_name.include?("M:")
-        @methods << m
+    doc.xpath('//member[starts-with(@name, "T:SharedLibrary.Service.")]').each do |tm|
+      methods = Array.new
+      class_name = tm.attribute("name").to_s.gsub(/T:/, "")
+      doc.xpath("//member[starts-with(@name, \"M:#{class_name}\")]").each do |mm|
+        methods << mm
       end
+      @class_list << Member.new(tm, methods)
     end
-    class_list << @member
-
-    @output = puts_class_list(class_list)
-  end
-
-
-
-
-
-  def puts_class_list class_list
-    output = ""
-    puts "#########"
-    puts class_list.size
-    class_list.each do |c|
-      puts c.classname
-      output = output + c.classname + "\r\n"
-      if c.fields && c.fields.size > 0
-        c.fields.each do |f|
-          puts "  " + f.attribute("name").to_s
-          output = output + "　　" + f.attribute("name").to_s + "\r\n"
-        end
-      end
-      if c.methods && c.methods.size > 0
-        c.methods.each do |method|
-          puts "  " + method.attribute("name").to_s
-          output = output + "　　" + method.attribute("name").to_s + "\r\n"
-        end
-      end
-    end
-    puts "#########"
-    output
   end
 end
